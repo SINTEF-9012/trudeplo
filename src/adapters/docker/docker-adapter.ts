a3f287a7b7591e7ee1daa5b61eaeef5f34163470
Hui Song <hui.song@sintef.no>
2022-08-08 12:43:20 +0200

    more folders

--- src/adapters/docker/docker-adapter.ts
+++ src/adapters/docker/docker-adapter.ts
@@ -1,8 +1,47 @@
 import Docker from 'dockerode'
 
-export async function createDockerEngine(engineModel:any){
-    const host = engineModel.host
-    const docker = new Docker({protocol:'http', host:host, port:2375 })
-    let images = await docker.listImages()
-    return images;
+export interface Container{
+    name: string;
+    image: string;
+    created: boolean;
+    running: boolean;
+}
+
+export interface ArtifactModel{
+    name?: string;
+    image: string;
+}
+
+export class DockerAdapter{
+    docker: Docker
+    containers: Container[] = []
+
+    constructor(engineModel:any){
+        const host = engineModel.host
+        this.docker = new Docker({protocol:'http', host:host, port:2375 })
+    }
+
+    async listImages(){
+        let images = await this.docker.listImages()
+        return images;
+    }
+
+    async createContainer(model: ArtifactModel){
+        const image = model.image
+        const name = model.name ?? 'trust_agent'
+        let containerStub = this.containers.find(item => item.name == name)
+            
+        if(! containerStub){
+            containerStub = {name: name, image: image, created: false, running: false}
+            this.containers.push(containerStub)
+        }
+
+        let container = await this.docker.createContainer({
+            name: name,
+            Image: image
+        })
+
+
+
+    }
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,9 +1,11 @@
-import { createDockerEngine } from "../src/adapters/docker/docker-adapter"
+import { DockerAdapter } from "../src/adapters/docker/docker-adapter"
 
 
 describe("toy tests",  ()=>{
+    // Not working under WSL
     it("Should always return some images", async ()=>{
-        const result = await createDockerEngine({host:'SINTEFPC9977.local'})
+        const docker = new DockerAdapter({host:'localhost'})
+        let result = await docker.listImages()
         expect(result.length).toBeGreaterThan(0)
     })
 })
