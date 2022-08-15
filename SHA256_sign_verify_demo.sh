8e38ab3e6e095d21d4bba937c956f6d5010b84af
Sintshuk <106529453+Sintshuk@users.noreply.github.com>
2022-08-15 12:38:04 +0200

    Create SHA256_sign_verify_demo.sh

---
+++ SHA256_sign_verify_demo.sh
@@ -0,0 +1,20 @@
+#generate key pair
+
+openssl ecparam -genkey -name secp384r1 > private_key.pem
+
+#show private key
+openssl pkcs8 -topk8 -in private_key.pem -nocrypt
+
+#public key, show
+openssl ec -in private_key.pem -pubout > public_key.pem
+cat public_key.pem
+
+#sign, private key
+openssl dgst -sha256 -sign private_key.pem TextAgent.txt > testkeypair.sig
+
+#verify, public key
+openssl dgst -sha256 -verify public_key.pem -signature testkeypair.sig TextAgent.txt 
+#Verified OK
+
+#COSIGN 
+#generate certicates
