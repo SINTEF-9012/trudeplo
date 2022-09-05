e4f5e7743969ef4303092b411537ded52290a691
Sintshuk <shukun.tokas@sintef.no>
2022-09-05 15:09:29 +0200

    minor

--- SignVerifyContainers/verify_agent_blob.js
+++ SignVerifyContainers/verify_agent_blob.js
@@ -1,13 +1,13 @@
 const { exec } = require("child_process");
 
 exec("cosign verify-blob --key /mnt/c/Users/shukunt/git/trudeplo/SignVerifyContainers/dummyDLT/cosign2.pub --signature /mnt/c/Users/shukunt/git/trudeplo/SignVerifyContainers/SignRepository/blob.sig TestAgent2.txt", (error, stdout, stderr) => {
-    if (error !== null) {
-        console.log('error: ' + error);
+    if (error) {
+        console.log('Error: ' + error);
+        return;
     }
     console.log("Standard Output: " + stdout + " ");
     console.log("Error Output: " + stderr + " ");
-
     //#region if (stderr !== null) {
       //#region   console.log("Error Output: " + stderr + " ");
-    //#endregion    }
+    //#endregion    }  
 });
