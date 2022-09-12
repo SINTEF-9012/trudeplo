7b48ffe79c9d07903dfa519548fe554ebfdb02f3
Sintshuk <shukun.tokas@sintef.no>
2022-09-12 12:55:46 +0200

    status check via filestreams

--- SignVerifyContainers/verify_agent_blob.js
+++ SignVerifyContainers/verify_agent_blob.js
@@ -1,19 +1,34 @@
 const { exec } = require("child_process");
-const verification_status = "Verified OK";
+const assert = require('assert');
+var fs1 = require('fs');
+var fs2 = require('fs');
+let expected_verification_status = "";
+let current_verification_status = "";
 
-exec("cosign verify-blob --key /mnt/c/Users/shukunt/git/trudeplo/SignVerifyContainers/dummyDLT/cosign2.pub --signature /mnt/c/Users/shukunt/git/trudeplo/SignVerifyContainers/SignRepository/blob.sig TestAgent2.txt", (error, stdout, stderr) => {
+exec("cosign verify-blob --key /mnt/c/Users/shukunt/git/trudeplo/SignVerifyContainers/dummyDLT/cosign2.pub --signature /mnt/c/Users/shukunt/git/trudeplo/SignVerifyContainers/SignRepository/blob.sig TestAgent2.txt > out.txt 2> error.txt", (error, stdout, stderr) => {
     if (error) {
       console.error('error: ' + error);
         return;
     }
-    console.log("stdout: " + stdout);
-    console.log(verification_status === stdout);
-    if(verification_status === stdout)
-      console.log("TA verified.");
-    else
-      console.log("TA cannot be verified.");
 
-      console.log("stdout: " + stdout);
-      console.log("verification status:" + verification_status);
-      console.log("Error Output: " + stderr);    
+    expected_verification_status = fs1.readFileSync('expected_status.txt', 'utf-8');
+    current_verification_status = fs2.readFileSync('error.txt', 'utf-8');
+    
+    console.log('Expected status:' + expected_verification_status);
+    console.log('Actual status:' +  current_verification_status);
+  
+    if(fs1.readFileSync('expected_status.txt', 'utf-8') === fs2.readFileSync('error.txt', 'utf-8'))
+      console.log('TA verified.');
+    else
+      console.log('TA cannot be verified.');   
+
+
+      //try{
+    //    assert.deepEqual(fs1.readFileSync('expected_status.txt', 'utf-8'), fs2.readFileSync('error.txt', 'utf-8'), "TA cannot be verified.")
+    //    console.log("TA verified.")
+    //}
+    //catch(error){
+    //    console.log(error.message)
+    //}
+   
 });
